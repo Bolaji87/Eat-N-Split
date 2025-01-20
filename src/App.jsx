c191e001006bf13cea5bae85c127dd50d1389016
bolaji87 <sakabolaji77@gmail.com>
2025-01-21 00:40:43 +0100

    feat: render all in Ap.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,29 +1,51 @@
 import React, { useState } from "react";
-import { initialFriends } from "./utils";
+
 import Friends from "./components/Friends";
 import FormAddFriend from "./components/FormAddFriend";
 import FormSplitBill from "./components/FormSplitBill";
-// import FormSplitBill from "./components/FormSplitBill";
+import Button from "./components/Button";
+import { initialFriends } from "./utils";
 
 function App() {
   const [friends, setFriends] = useState(initialFriends);
+  const [showAddFriend, setShowAddFriend] = useState(false);
+  const [selectedFriend, setSelctedFriend] = useState(null);
 
-  function handleAddFriend(friend) {
-    setFriends((friends) => [...friends, friend]);
+  function handleShowAddFriend() {
+    setShowAddFriend((showFriend) => !showFriend);
+  }
+  function handleAddFriend(newFriend) {
+    setFriends((friends) => [...friends, newFriend]);
+    setShowAddFriend(false);
+  }
+
+  function handleSelection(friend) {
+    // setSelctedFriend(friend);
+    setSelctedFriend((prev) => (prev?.id === friend.id ? null : friend));
+    setShowAddFriend(false);
   }
 
   return (
     <div className="app">
       <div className="sidebar">
-        <ul>
-          {friends.map(function (friend) {
-            return <Friends key={friend.id} friend={friend} />;
-          })}
-        </ul>
+        <Friends
+          friends={friends}
+          onSelection={handleSelection}
+          selectedFriend={selectedFriend}
+        />
+        {showAddFriend && <FormAddFriend onAddFriend={handleAddFriend} />}
+        <Button onClick={handleShowAddFriend}>
+          {showAddFriend ? "Close" : "Add Friend"}
+        </Button>
       </div>
 
-      <FormSplitBill />
-      {friends && <FormAddFriend onAddFriend={handleAddFriend} />}
+      {selectedFriend && (
+        <FormSplitBill
+          selectedFriend={selectedFriend}
+          friends={friends}
+          setFriends={setFriends}
+        />
+      )}
     </div>
   );
 }
