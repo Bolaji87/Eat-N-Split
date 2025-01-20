66da5cb4362662722c13bc2cb3f47fd623693b23
bolaji87 <sakabolaji77@gmail.com>
2025-01-20 13:04:36 +0100

    feat: create formAddFriend component

---
+++ src/components/FormAddBill.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+function FormAddBill() {
+  return (
+    <div>
+      <label htmlFor=""></label>
+      <input type="text" />
+      <label htmlFor=""></label>
+      <input type="text" />
+    </div>
+  );
+}
+
+export default FormAddBill;
