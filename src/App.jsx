75b48ee97202188d357e34f3ff171557bd017d02
bolaji87 <sakabolaji77@gmail.com>
2025-01-20 13:00:47 +0100

    feat: render friends component as list

--- src/App.jsx
+++ src/App.jsx
@@ -1,28 +1,19 @@
-const initialFriends = [
-  {
-    id: 118836,
-    name: "Clark",
-    image: "https://i.pravatar.cc/48?u=118836",
-    balance: -7,
-  },
-  {
-    id: 933372,
-    name: "Sarah",
-    image: "https://i.pravatar.cc/48?u=933372",
-    balance: 20,
-  },
-  {
-    id: 499476,
-    name: "Anthony",
-    image: "https://i.pravatar.cc/48?u=499476",
-    balance: 0,
-  },
-];
-
 import React from "react";
+import { initialFriends } from "./utils";
+import Friends from "./components/Friends";
 
 function App() {
-  return <div>Eat and split</div>;
+  return (
+    <div className="app">
+      <div className="sidebar">
+        <ul>
+          {initialFriends.map(function (friend) {
+            return <Friends key={friend.id} friend={friend} />;
+          })}
+        </ul>
+      </div>
+    </div>
+  );
 }
 
 export default App;
