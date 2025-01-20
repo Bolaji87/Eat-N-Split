47f9fd732a194a3c436e30b61a1e00c99b073880
bolaji87 <sakabolaji77@gmail.com>
2025-01-21 00:39:59 +0100

    feat: update friends

--- src/components/Friends.jsx
+++ src/components/Friends.jsx
@@ -1,32 +1,46 @@
 import React from "react";
 import Button from "./Button";
 
-function Friends({ friend }) {
+function Friends({ friends, selectedFriend, onSelection }) {
   return (
-    <li>
+    <ul>
+      {friends.map(function (friend) {
+        return (
+          <Friend
+            key={friend.id}
+            friend={friend}
+            selectedFriend={selectedFriend}
+            onSelection={onSelection}
+          />
+        );
+      })}
+    </ul>
+  );
+}
+
+function Friend({ friend, selectedFriend, onSelection }) {
+  const isSelected = selectedFriend?.id === friend.id;
+  return (
+    <li className={isSelected ? "selected" : ""}>
       <img src={friend.image} alt="" />
       <div>
         <h3>{friend.name}</h3>
-        <p
-          className={`${
-            friend.balance < 1 && friend.balance !== 0 ? "red" : ""
-          }`}
-        >
-          {friend.balance < 1 &&
-            friend.balance !== 0 &&
-            `you owe ${friend.name} ${friend.balance}`}
+        <p className={`${friend.balance < 0 ? "red" : ""}`}>
+          {friend.balance < 0 &&
+            `you owe ${friend.name} ${Math.abs(friend.balance)}`}
         </p>
 
-        <p className={`${friend.balance >= 1 ? "green" : ""}`}>
-          {friend.balance >= 1 && `${friend.name} owes you ${friend.balance}`}
+        <p className={`${friend.balance > 0 ? "green" : ""}`}>
+          {friend.balance > 0 && `${friend.name} owes you ${friend.balance}`}
         </p>
 
         <p>{friend.balance === 0 && `You and ${friend.name} are even`}</p>
       </div>
 
-      <Button>Select</Button>
+      <Button onClick={() => onSelection(friend)}>
+        {isSelected ? "Close" : "Select"}
+      </Button>
     </li>
   );
 }
-
 export default Friends;
